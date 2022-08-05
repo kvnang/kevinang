8693b4bbf47a50276af115d299b07a39f4cfa744
Kevin Ang <51168758+kvnang@users.noreply.github.com>
2022-08-04 20:35:52 -0500

    fix: remove edge functions

--- svelte.config.js
+++ svelte.config.js
@@ -8,9 +8,7 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter({
-			edge: true
-		}),
+		adapter: adapter(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		// target: '#svelte'
 		prerender: {
