9825b0899ffe924cfdea9ff67f61783f1a734d1d
Kevin Ang <51168758+kvnang@users.noreply.github.com>
2022-08-06 06:55:43 -0500

    fix: accessibility

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -1,14 +1,21 @@
-<script lang="ts">
+<!-- <script lang="ts">
 	import { page } from '$app/stores';
 	import logo from './logo.svg';
-</script>
+</script> -->
 
 <header>
 	<div class="container">
 		<div class="corner">
 			<a href="/" class="logo">
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 180"
-					><path
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 200 180"
+					width="200"
+					height="180"
+					aria-labelledby="header-logo-title"
+				>
+					<title id="header-logo-title">Kevin Ang</title>
+					<path
 						d="m76.52 57.48 38.82-46a29.15 29.15 0 0 1 21.73-9.62h.87V0H92.25v1.78h1.92c5.51 0 8.1 5 4.74 9.47L45.2 78V14.38A12.75 12.75 0 0 1 58 2h2.51V0H0v2h2.57a12.74 12.74 0 0 1 12.74 12.42v110.39a12.76 12.76 0 0 1-12.76 12.51H0v2h9.86Z"
 						fill="#fff"
 					/><path
@@ -42,8 +49,15 @@
 			<ul>
 				<li>
 					<a href="https://github.com/kvnang" target="_blank" rel="noopener noreferrer">
-						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32.58 31.77" width="33" height="32"
-							><path
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 32.58 31.77"
+							width="33"
+							height="32"
+							aria-labelledby="github-logo"
+						>
+							<title id="github-logo">View My GitHub Profile</title>
+							<path
 								fill="#fff"
 								fill-rule="evenodd"
 								d="M16.29 0a16.29 16.29 0 0 0-5.15 31.75c.82.15 1.11-.36 1.11-.79v-2.77C7.7 29.18 6.74 26 6.74 26a4.36 4.36 0 0 0-1.81-2.39c-1.47-1 .12-1 .12-1a3.43 3.43 0 0 1 2.49 1.68 3.48 3.48 0 0 0 4.74 1.36 3.46 3.46 0 0 1 1-2.18c-3.62-.41-7.42-1.81-7.42-8a6.3 6.3 0 0 1 1.67-4.37 5.94 5.94 0 0 1 .16-4.31s1.37-.44 4.48 1.67a15.41 15.41 0 0 1 8.16 0c3.11-2.11 4.47-1.67 4.47-1.67a5.91 5.91 0 0 1 .2 4.28 6.3 6.3 0 0 1 1.67 4.37c0 6.26-3.81 7.63-7.44 8a3.85 3.85 0 0 1 1.11 3v4.47c0 .53.29.94 1.12.78A16.29 16.29 0 0 0 16.29 0Z"
