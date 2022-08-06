f8222caa8fb9cdc59bd01913a0dd7f81aeec2654
Kevin Ang <51168758+kvnang@users.noreply.github.com>
2022-08-06 16:47:42 -0500

    fix: env

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,9 +1,8 @@
-import { browser } from '$app/env';
 import { removeTrailingSlash } from './utils';
 
 export const config = {
 	/**
 	 * Site URL without trailing slash
 	 */
-	siteUrl: removeTrailingSlash(browser ? import.meta.env.URL : process.env.URL)
+	siteUrl: removeTrailingSlash(import.meta.env.VITE_SITE_URL)
 };
