f8070d0b1a133fa4257189a36ad253865c9263ab
Kevin Ang <51168758+kvnang@users.noreply.github.com>
2022-08-08 21:42:17 -0500

    feat: add srcset

--- src/lib/image/Image.svelte
+++ src/lib/image/Image.svelte
@@ -1,21 +1,22 @@
 <script lang="ts">
 	import { dev } from '$app/env';
 
-	export let src: string;
-	export let alt: string;
+	export let src: string | null;
+	export let alt = '';
 	export let srcset: string | null = null;
 	export let sizes: string | null = null;
 
 	if (!dev) {
 		const deviceSizes = [640, 750, 828, 1080, 1200, 1920, 2048, 3840];
 		// TODO: add Cloudinary params
-		src = `https://res.cloudinary.com/kevinang/image/upload/f_auto/ka${src}`;
 		srcset = deviceSizes
 			.map(
 				(size) =>
 					`https://res.cloudinary.com/kevinang/image/upload/f_auto,w_${size}/ka${src} ${size}w`
 			)
 			.join(', ');
+		src = null;
+		// src = `https://res.cloudinary.com/kevinang/image/upload/f_auto/ka${src}`;
 		sizes = `(min-width: 1440px) 50vw, (min-width: 768px) 25vw, 100vw`;
 	}
 </script>
