b12a4dacc951f7e1e69f8e7880a05474b88dbfc3
Kevin Ang <51168758+kvnang@users.noreply.github.com>
2022-08-19 13:15:11 -0500

    upgrade

--- src/lib/image/Image.svelte
+++ src/lib/image/Image.svelte
@@ -1,22 +1,31 @@
 <script lang="ts">
 	import { dev } from '$app/env';
+	import { config } from '$lib/config';
+	import { removeLeadingSlash } from '$lib/utils';
 
-	export let src: string | null;
+	export let src: string | null | undefined;
 	export let alt = '';
-	export let srcset: string | null = null;
-	export let sizes: string | null = null;
+	export let srcset: string | null | undefined = null;
+	export let sizes: string | null | undefined = null;
 
-	if (!dev) {
+	if (!dev && src) {
+		const { siteUrl } = config;
 		const deviceSizes = [640, 750, 828, 1080, 1200, 1920, 2048, 3840];
-		// TODO: add Cloudinary params
+
+		// Remove hostname
+		if (siteUrl) {
+			src = src?.replace(siteUrl, '');
+		}
+
+		src = removeLeadingSlash(src);
+
 		srcset = deviceSizes
 			.map(
 				(size) =>
-					`https://res.cloudinary.com/kevinang/image/upload/f_auto,w_${size}/ka${src} ${size}w`
+					`https://res.cloudinary.com/kevinang/image/upload/f_auto,w_${size}/ka/${src} ${size}w`
 			)
 			.join(', ');
 		src = null;
-		// src = `https://res.cloudinary.com/kevinang/image/upload/f_auto/ka${src}`;
 		sizes = `(min-width: 1440px) 50vw, (min-width: 768px) 25vw, 100vw`;
 	}
 </script>
