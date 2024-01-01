80d4fe5418ea1845fa7ef08746272eb4e677eea8
Kevin Ang <51168758+kvnang@users.noreply.github.com>
2023-12-31 19:58:03 -0500

    fix: url

--- web/lib/constants.ts
+++ web/lib/constants.ts
@@ -1,4 +1,8 @@
-export const BASE_URL =
+export const BASE_HOST =
   process.env.NEXT_PUBLIC_URL ||
   process.env.NEXT_PUBLIC_VERCEL_URL ||
-  "http://localhost:3000";
+  "localhost:3000";
+
+export const BASE_URL = BASE_HOST.startsWith("localhost")
+  ? `http://${BASE_HOST}`
+  : `https://${BASE_HOST}`;
